4cfaf5b41536c1aa1f91125b1621287aeb029e12
weishaodaren <weishaodaren@gmail.com>
2021-12-23 17:05:03 +0800

    setData

---
+++ src/hello/setData.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Result struct {
+	Code    int    `json:"code"`
+	Message string `json:"msg"`
+}
+
+func main() {
+	res := Result{Code: 200, Message: "success"}
+	toJson(&res)
+
+	setData(&res)
+	toJson(&res)
+}
+
+func setData(res *Result) {
+	res.Code = 500
+	res.Message = "fail"
+}
+
+func toJson(res *Result) {
+	jsons, errs := json.Marshal(res)
+	if errs != nil {
+		fmt.Println("json marshal error:", errs)
+	}
+	fmt.Println("json data:", string(jsons))
+}
