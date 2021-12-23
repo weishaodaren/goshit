9e96202024cbca9ffe267801ecca94bcf3182528
weishaodaren <weishaodaren@gmail.com>
2021-12-23 15:43:58 +0800

    update

--- src/hello/array.go
+++ src/hello/array.go
@@ -8,15 +8,25 @@ func main() {
 	var arr_1 [5]int
 	fmt.Println(arr_1)
 
-	var arr_2 = [5] int {1, 2, 3, 4, 5}
+	var arr_2 = [5]int{1, 2, 3, 4, 5}
 	fmt.Println(arr_2)
 
-	arr_3 := [5] int {1, 2, 3, 4, 5}
+	arr_3 := [5]int{1, 2, 3, 4, 5}
 	fmt.Println(arr_3)
 
-	arr_4 := [...] int {1, 2, 3, 4, 5, 6, 7, 8}
+	arr_4 := [...]int{1, 2, 3, 4, 5, 6, 7, 8}
 	fmt.Println(arr_4)
 
-	arr_5 := [5] int {0:3, 1:5, 4:6}
+	arr_5 := [5]int{0: 3, 1: 5, 4: 6}
 	fmt.Println(arr_5)
+
+	var arr_6 = [3][5]int{{1, 2, 3, 4, 5}, {9, 8, 7, 6, 5}, {3, 4, 5, 6, 7}}
+	fmt.Println(arr_6)
+
+	modifyArr(&arr_3)
+	fmt.Println(arr_3)
+}
+
+func modifyArr(a *[5] int) {
+	a[1] = 20
 }
