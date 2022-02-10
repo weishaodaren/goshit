23a796ea13f0ccb7eae8009cbdffff3e6726697e
weishaodaren <weishaodaren@gmail.com>
2022-02-10 11:12:21 +0800

    output runtime.Goos & output os.Getenv

---
+++ src/bye/variable.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b *int
+	var c int
+	var d bool
+	var str string
+
+	var (
+		e int
+		f bool
+		g string
+	)
+
+	aa = 15
+	bb = false
+
+	// var identifier [type] = value
+	var aA int = 15
+	var i = 5
+	var bB bool = false
+	var strStr string = "Go says hello to the world"
+
+	cC := 1
+}
