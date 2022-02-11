a6463252af2e430fb98b90e812549f9f064b5da9
weishaodaren <weishaodaren@gmail.com>
2022-02-11 16:02:10 +0800

    Replace

--- src/bye/index_in_string.go
+++ src/bye/index_in_string.go
@@ -13,4 +13,6 @@ func main() {
 
 	fmt.Printf("The position of the last instance og \"Hi\" is: ")
 	fmt.Printf("最后出现的位置 %d\n", strings.LastIndex(str, "Hi"))
+
+	fmt.Printf("Replace %s", strings.Replace(str, "Marc", "weishaodaren", 1))
 }
