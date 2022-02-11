bda7de61b26073fe1f86c82782d62a2c812da361
weishaodaren <weishaodaren@gmail.com>
2022-02-11 15:56:55 +0800

    Index

---
+++ src/bye/index_in_string.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str string = "Hi, I'm Marc, Hi."
+
+	fmt.Printf("The position of \"Marc\" is: ")
+	fmt.Printf("首次出现的位置 %d\n", strings.Index(str, "Marc"))
+
+	fmt.Printf("The position of the last instance og \"Hi\" is: ")
+	fmt.Printf("最后出现的位置 %d\n", strings.LastIndex(str, "Hi"))
+}
