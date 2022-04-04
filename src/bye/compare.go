7ee4525fe9acfaaecbe91765734fbdd048d6df0e
weishaodaren <weishaodaren@gmail.com>
2022-04-04 14:30:43 +0800

    compare

--- src/bye/compare.go
+++ src/bye/compare.go
@@ -1,1 +1,19 @@
-func Compare(a, b[]byte)
+package main
+
+func Compare(a, b []byte) int {
+	for i := 0; i < len(a) && i < len(b); i++ {
+		switch {
+		case a[i] > b[i]:
+			return 1
+		case a[i] < b[i]:
+			return -1
+		}
+	}
+	switch {
+	case len(a) < len(b):
+		return -1
+	case len(a) > len(b):
+		return 1
+	}
+	return 0
+}
