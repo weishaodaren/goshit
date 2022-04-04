7c23815f92af6d09a14162529b6d609925c6e2f4
weishaodaren <weishaodaren@gmail.com>
2022-04-04 23:12:47 +0800

    update

---
+++ src/bye/maps_forrange2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	items := make([]map[int]int, 5)
+	for i := range items {
+		items[i] = make(map[int]int, 1)
+		items[i][1] = 2
+	}
+	fmt.Printf("Version A : Value if items: %v\n", items)
+}
+
