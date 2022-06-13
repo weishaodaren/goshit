59e063b65a8ee6e491f6c6e96a0531e95b6f996e
weishaodaren <weishaodaren@gmail.com>
2022-06-13 16:10:52 +0800

    gtpl

--- src/web/web_example1.go
+++ src/web/web_example1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
@@ -32,12 +33,26 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "WEISHAODAREN ROUte") //这个写入到w的是输出到客户端的
+	fmt.Fprintf(w, "WEISHAODAREN Route") //这个写入到w的是输出到客户端的
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		log.Println(t.Execute(w, nil))
+	} else {
+		//请求的是登录数据，那么执行登录的逻辑判断
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
 }
 
 func main() {
-	mux := &MyMux{}
-	err := http.ListenAndServe(":9090", mux) //设置监听的端口
+	// mux := &MyMux{}
+	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
